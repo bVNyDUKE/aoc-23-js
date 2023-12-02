338d030d446a78fa749ae6cd7fb748771763da6d
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-12-02 19:34:43 +0100

    day 1 part 2

--- day1/main.js
+++ day1/main.js
@@ -1,5 +1,37 @@
 const fs = require("fs");
 
+const re = new RegExp(
+  "(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)"
+);
+
+const vals = {
+  one: 1,
+  1: 1,
+  two: 2,
+  2: 2,
+  three: 3,
+  3: 3,
+  four: 4,
+  4: 4,
+  five: 5,
+  5: 5,
+  six: 6,
+  6: 6,
+  seven: 7,
+  7: 7,
+  eight: 8,
+  8: 8,
+  nine: 9,
+  9: 9,
+};
+
+function checkNumber(string) {
+  const res = re.exec(string);
+  if (res) {
+    return vals[res[0]];
+  }
+}
+
 const res = fs
   .readFileSync("./input.txt")
   .toString()
@@ -9,11 +41,8 @@ const res = fs
     let last = null;
     for (let i = 0; i < line.length; i++) {
       if (first && last) break;
-      if (!first && parseInt(line[i])) {
-        first ??= parseInt(line[i]);
-      }
-      if (!last && parseInt(line[line.length - 1 - i]))
-        last ??= parseInt(line[line.length - 1 - i]);
+      first ??= checkNumber(line.slice(0, i + 1));
+      last ??= checkNumber(line.slice((i + 1) * -1));
     }
     res += first * 10 + last;
     return res;
