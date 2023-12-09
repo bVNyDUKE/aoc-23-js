49071ad73ab79dbdd8e7c242007cd2f589eb2335
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-12-09 23:25:58 +0100

    day 8 part 2

--- day8/main.ts
+++ day8/main.ts
@@ -2,33 +2,47 @@ import * as fs from "node:fs";
 
 const f = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-const commands = f
-  .shift()
-  ?.split("")
+const commands = f[0]
+  .split("")
   .filter(Boolean)
-  .map((d) => (d === "L" ? 0 : 1)) as (1 | 0)[];
+  .map((d) => (d === "L" ? 0 : 1));
 
-f.shift();
-const path = f.reduce(
-  (p, curr) => {
-    const [key, dirs] = curr.split(" = ");
-    p[key] = dirs.slice(1, -1).split(", ");
-    return p;
-  },
-  {} as Record<string, string[]>,
-);
+const path = f.slice(2).reduce<Record<string, string[]>>((p, curr) => {
+  console.log("CURRENT", curr);
+  const [key, dirs] = curr.split(" = ");
+  p[key] = dirs
+    .slice(1, -1)
+    .trim()
+    .split(", ")
+    .map((d) => d.trim());
+  return p;
+}, {});
 
 console.log("PATH", path);
 console.log(commands);
 
-let key = "AAA";
-let steps = 0;
+const keys = Object.keys(path).filter((k) => k.endsWith("A"));
+const ts = [];
 
-while (key !== "ZZZ") {
-  console.log(key);
-  const dir = commands[steps % commands.length];
-  key = path[key][dir];
-  ++steps;
+function gcd(x: number, y: number): number {
+  if (y === 0) return x;
+  return gcd(y, x % y);
 }
 
-console.log(steps);
+function lcm(x: number, y: number): number {
+  return (x * y) / gcd(x, y);
+}
+
+for (const k of keys) {
+  let steps = 0;
+  let key = k;
+  while (!key.endsWith("Z")) {
+    key = path[key][commands[steps % commands.length]];
+    ++steps;
+  }
+  ts.push(steps);
+}
+
+const res = ts.reduce((prev, curr) => lcm(prev, curr));
+console.log(ts);
+console.log(res);
