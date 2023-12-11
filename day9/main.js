fb1c79c67692676d9c8037c07d8cedcd7e7c4f6e
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-12-12 00:13:21 +0100

    day9

---
+++ day9/main.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+function getDiff(line) {
+  const sum = [line];
+  while (!sum[sum.length - 1].every((l) => l === 0)) {
+    const l = sum[sum.length - 1];
+    const res = [];
+    for (let i = 0; i < l.length - 1; i++) {
+      res.push((l[i + 1] || 0) - (l[i] || 0));
+    }
+    sum.push(res);
+  }
+
+  const res = sum.reduceRight((t, _, i, arr) => {
+    return t + arr[i].pop();
+  }, 0);
+
+  return res;
+}
+
+const res = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .split("\n")
+  .filter(Boolean)
+  .map((line) => {
+    return line.split(" ").map((l) => parseInt(l));
+  })
+  .reduce((prev, curr) => {
+    return prev + getDiff(curr);
+  }, 0);
+
+console.log(res);
